5f8d5d4d3339caa8cae458902657ae15cbe103fa
jordanoutlawgraphix-svg <jordan.outlawgraphix@gmail.com>
2026-03-31 17:08:50 -0500

    Initial commit - Fast Threads DTF Workflow Manager

---
+++ setup_git.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\TJ\Desktop\fast-threads-dtf"
+"C:\Program Files\Git\cmd\git.exe" init
+"C:\Program Files\Git\cmd\git.exe" add -A
+"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit - Fast Threads DTF Workflow Manager"
+"C:\Program Files\Git\cmd\git.exe" status
+echo DONE
